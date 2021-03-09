c9a30106f0f2cb9e1ae5d41f49c9cb8323503ce1
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-03-09 13:56:51 +0200

    first commit

---
+++ 01 - path.js
@@ -0,0 +1,9 @@
+﻿const path = require('path')
+
+console.log('__dirname:', __dirname)
+console.log('__filename:', __filename)
+
+console.log('Filename:', path.basename(__filename))
+console.log(' Dirname:', path.dirname(__filename))
+console.log(' Extname:', path.extname(__filename))
+console.log(' Parse:', path.parse(__filename))
